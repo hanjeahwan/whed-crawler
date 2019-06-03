3a1d7de4842190a53275a1f665ab6ce41194236a
hanjeahwan <hanjeahwan@gmail.com>
2019-06-03 09:41:18 +0800

    chalk

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
 import { countries } from './countries';
@@ -9,11 +10,11 @@ try {
     await crawler.start('https://whed.net/results_institutions.php', true);
 
     for (const country of countries) {
-      console.log('log:  Processing ->', country);
+      console.log(chalk.bgCyan(' LOG '), chalk.cyan('Processing', country));
       const results = await crawler.search(country);
       const filePath = path.join(process.cwd(), "results", `${country}.json`);
       await fs.writeFileSync(filePath, JSON.stringify(results, null, 0), 'utf-8');
-      console.log('log: Result ->', country, ` -> total -> ${results.length}`);
+      console.log(chalk.bgGreen(' RESULT '), chalk.green(`${country} total result ${results.length}`));
     }
     console.timeEnd('Crawling Time');
     crawler.browser.close();
